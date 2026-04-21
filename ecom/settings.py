9703fb26181bca4dce488190b07e9b3bae598b05
Jaspreet Singh <sainij1133@gmail.com>
2026-04-21 20:54:40 +1000

    prepare for railway

--- ecom/settings.py
+++ ecom/settings.py
@@ -85,7 +85,7 @@ WSGI_APPLICATION = 'ecom.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_PUBLIC_URL"))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_PUBLIC_URL'),conn_max_age=600,ssl_require=True)
     
 }
 
